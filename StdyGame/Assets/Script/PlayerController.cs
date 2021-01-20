8397c987564d4a51aa1686c6eee2105246266a4b
Master <shiftthainguyen@gmail.com>
2021-01-20 11:06:39 +0700

    Add file

---
+++ StdyGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float movementSpeed = 10.0f;
+    private float movementInputDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+    private void FixedUpdate()
+    {
+        ApplyInput();
+    }
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+    }
+    private void ApplyInput()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }
+}
