6e5d7c8e185efd667388f6d60fc9472414d9b33a
Master <shiftthainguyen@gmail.com>
2021-01-20 21:09:57 +0700

    Add file part 5

--- StdyGame/Assets/Script/PlayerController.cs
+++ StdyGame/Assets/Script/PlayerController.cs
@@ -8,20 +8,35 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private float movementInputDirection;
     private int amountOfJumpLeft;
+    private int facingDirection = 1;
 
     public float movementSpeed = 10.0f;  
     public float jumpForce = 16.0f;   
     public float groundCheckRadius;
+    public float wallCheckDistance;
+    public float wallSildeSpeed;
+    public float movementForceInAir;
+    public float airDragMultiplier =0.95f;
+    public float variableJumpHeightMultiplier =0.5f;
+    public float wallHopForce;
+    public float wallJumpForce;
 
 
     private bool isGrounded;
     private bool isFacingRight = true;
     private bool isWalking;
     private bool canJump;
+    private bool isTouchingWall;
+    private bool isWallSilding;
 
     public int amountOfJump = 1;
 
+    public Vector2 wallHopDirection;
+    public Vector2 wallJumpDirection;
+
     public Transform groubdCheck;
+    public Transform wallCheck;
+
     public LayerMask whatIsGround;
 
     // Start is called before the first frame update
@@ -30,6 +45,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         amountOfJumpLeft = amountOfJump;
+        wallHopDirection.Normalize();
+        wallJumpDirection.Normalize();
     }
 
     // Update is called once per frame
@@ -39,19 +56,32 @@ public class PlayerController : MonoBehaviour
         CheckMovementDirection();
         UpdateAnimation();
         CheckCanJump();
+        CheckWallSilding();
     }
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
         ApplyInput();
         CheckSurroundings();
     }
+    private void CheckWallSilding()
+    {
+        if(isTouchingWall && !isGrounded && Mathf.Abs(rb.velocity.y) > 0)
+        {
+            isWallSilding = true;           
+        }
+        else
+        {
+            isWallSilding = false;          
+        }
+    }
     private void CheckSurroundings()
     {
         isGrounded = Physics2D.OverlapCircle(groubdCheck.position, groundCheckRadius, whatIsGround);
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance,whatIsGround);
     }
     private void CheckCanJump()
     {
-        if( isGrounded&& rb.velocity.y <=0)
+        if((isGrounded && rb.velocity.y <= 0) || isWallSilding )
         {
             amountOfJumpLeft = amountOfJump;          
         }
@@ -89,35 +119,84 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isGrounded", isGrounded);
         anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetFloat("speedMovement", Mathf.Abs(movementInputDirection));
+        anim.SetBool("isWallSliding", isWallSilding);
+
     }
     private void Flip()
     {
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0, 180, 0);
+        if (!isWallSilding)
+        {
+            facingDirection *= -1;
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+        }  
     }
     private void CheckInput()
     {
         movementInputDirection = Input.GetAxisRaw("Horizontal");
+
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+        }
     }
     private void Jump()
     {
-        if (canJump)
+        if (canJump && !isWallSilding)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             amountOfJumpLeft--;
+        }else if(isWallSilding && movementInputDirection ==0 && canJump) //wall hop
+        {
+            isWallSilding = false;
+            amountOfJumpLeft--;
+            Vector2 forceToAdd = new Vector2(wallHopForce * wallHopDirection.x * -facingDirection, wallHopForce * wallHopDirection.y);
+            rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+        }else if((isWallSilding || isTouchingWall) && movementInputDirection != 0 && canJump)
+        {
+            isWallSilding = false;
+            amountOfJumpLeft--;
+            Vector2 forceToAdd = new Vector2(wallJumpForce * wallJumpDirection.x * movementInputDirection, wallJumpForce * wallJumpDirection.y);
+            rb.AddForce(forceToAdd, ForceMode2D.Impulse);
         }
-        
     }
     private void ApplyInput()
     {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        }
+        else if (!isGrounded && !isWallSilding && movementInputDirection != 0) 
+        {
+            Vector2 foreToAdd = new Vector2(movementForceInAir * movementInputDirection, 0);
+            rb.AddForce(foreToAdd);
+            if (Mathf.Abs(rb.velocity.x) > movementSpeed)
+            {
+                rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+            }
+        }       
+        else if(!isGrounded && !isWallSilding && movementInputDirection == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
+        }
+        if (isWallSilding)
+        {
+            if(rb.velocity.y < -wallSildeSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSildeSpeed);
+            }
+        }
     }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(groubdCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+
     }
 }
